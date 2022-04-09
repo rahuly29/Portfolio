2946954903265ed715474dc22ffc4b8583abb9b6
RahulY29 <yadavrahul3298@gmail.com>
2022-04-09 11:09:58 +0530

    scroll behaviour

--- main.js
+++ main.js
@@ -10,3 +10,16 @@ window.onscroll = () => {
   menu.classList.remove("bx-x");
   navbar.classList.remove("active");
 };
+
+const sr = ScrollReveal({
+  distance: "60px",
+  duration: 2500,
+  reset: true,
+});
+
+sr.reveal(".home-text", { delay: 200, origin: "top" });
+sr.reveal(".home-image", { delay: 400, origin: "top" });
+sr.reveal(".about, .services, .cta, .resume, .contact, .copyright", {
+  delay: 200,
+  origin: "top",
+});
